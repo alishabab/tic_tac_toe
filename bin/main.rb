c1dc11353a801e59f2d8aa2e5d29afcb80846eea
Murilo Roque <muriloengqui@gmail.com>
2020-04-13 11:01:49 -0300

    Added moves inputs and win/lose/tie displays

--- bin/main.rb
+++ bin/main.rb
@@ -11,14 +11,30 @@ def display_grid(board)
   end
 end
 
+colors = ["X", "O"]
+
 puts "Welcome to tic-tac-toe game"
+display_grid(board)
 puts "Enter Player-1 name:"
 player_1 = gets.chomp
 puts "Please select X or O"
 color_1 = gets.chomp
+colors.delete(color_1)
 puts "Enter Player-2 name:"
 player_2 = gets.chomp
+color_2 = colors.push()
 
 
-puts "#{player_1}, #{player_2}, #{color_1}"
+puts "#{player_1}, please input your move, your color is #{color_1}:"
+puts move_1 = gets.chomp
+
 display_grid(board)
+
+puts "#{player_2}, please input your move, your color is #{color_2}:"
+puts move_2 = gets.chomp
+
+display_grid(board)
+
+puts "Game Over, #{player_1} won, congratulations!"
+puts "Game Over, #{player_2} won, congratulations!"
+puts "Game Over, it's a tie, too bad!"
