9e0a29eb799098fefedceed1b6d318fd75f77c81
Murilo Roque <muriloengqui@gmail.com>
2020-04-13 11:33:09 -0300

    Fixed linter errors

--- bin/main.rb
+++ bin/main.rb
@@ -11,30 +11,31 @@ def display_grid(board)
   end
 end
 
-colors = ["X", "O"]
+colors = %w[X O]
 
-puts "Welcome to tic-tac-toe game"
+puts 'Welcome to tic-tac-toe game'
 display_grid(board)
-puts "Enter Player-1 name:"
-player_1 = gets.chomp
-puts "Please select X or O"
-color_1 = gets.chomp
-colors.delete(color_1)
-puts "Enter Player-2 name:"
-player_2 = gets.chomp
-color_2 = colors.push()
+puts 'Enter Player-1 name:'
+player1 = gets.chomp
+puts 'Please select X or O'
+color1 = gets.chomp
+colors.delete(color1)
+puts 'Enter Player-2 name:'
+player2 = gets.chomp
+color2 = colors.push
 
-
-puts "#{player_1}, please input your move, your color is #{color_1}:"
-puts move_1 = gets.chomp
-
-display_grid(board)
-
-puts "#{player_2}, please input your move, your color is #{color_2}:"
-puts move_2 = gets.chomp
+puts "#{player1}, please input your move, your color is #{color1}:"
+move1 = gets.chomp
+puts move1
 
 display_grid(board)
 
-puts "Game Over, #{player_1} won, congratulations!"
-puts "Game Over, #{player_2} won, congratulations!"
+puts "#{player2}, please input your move, your color is #{color2}:"
+move2 = gets.chomp
+puts move2
+
+display_grid(board)
+
+puts "Game Over, #{player1} won, congratulations!"
+puts "Game Over, #{player2} won, congratulations!"
 puts "Game Over, it's a tie, too bad!"
