ba8b82519bc85ba473dd0991c6722d53befd47af
Shabab Ali <shababsaifi@gmail.com>
2020-04-13 19:20:12 +0530

    added name inputs

--- bin/main.rb
+++ bin/main.rb
@@ -1,6 +1,8 @@
+#!/usr/bin/env ruby
+
 board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-def display_grid (board)
+def display_grid(board)
   board.each do |n|
     n.each do |j|
       print "#{j} "
@@ -9,4 +11,14 @@ def display_grid (board)
   end
 end
 
+puts "Welcome to tic-tac-toe game"
+puts "Enter Player-1 name:"
+player_1 = gets.chomp
+puts "Please select X or O"
+color_1 = gets.chomp
+puts "Enter Player-2 name:"
+player_2 = gets.chomp
+
+
+puts "#{player_1}, #{player_2}, #{color_1}"
 display_grid(board)
