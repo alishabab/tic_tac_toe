61ef88c99a7b2d3666d9ff788cce5c6f6734bdfe
Murilo Roque <muriloengqui@gmail.com>
2020-04-14 07:50:40 -0300

    Added a display method on main.rb

--- lib/board.rb
+++ lib/board.rb
@@ -3,13 +3,4 @@ class Board
   def initialize(arr)
     @arr = arr
   end
-
-  def display_grid
-    @arr.each do |n|
-      n.each do |j|
-        print "#{j} "
-      end
-      print "\n"
-    end
-  end
 end
