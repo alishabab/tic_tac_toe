1f356f5925509298fb8b4ed0e15bd5c4fbd17a99
Shabab Ali <shababsaifi@gmail.com>
2020-04-14 18:13:31 +0530

    Added logic to check valid input

--- bin/main.rb
+++ bin/main.rb
@@ -1,24 +1,47 @@
 #!/usr/bin/env ruby
+require_relative '../lib/board.rb'
 
-board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-def display_grid(board)
-  board.each do |n|
-    n.each do |j|
-      print "#{j} "
-    end
-    print "\n"
-  end
-end
+# def display_grid(board)
+#   board.each do |n|
+#     n.each do |j|
+#       print "#{j} "
+#     end
+#     print "\n"
+#   end
+# end
 
 puts "Welcome to tic-tac-toe game"
+board = Board.new
 puts "Enter Player-1 name:"
-player_1 = gets.chomp
+name_1 = gets.chomp
 puts "Please select X or O"
 color_1 = gets.chomp
+color_2 = board.get_color(color_1)
+puts color_2
 puts "Enter Player-2 name:"
-player_2 = gets.chomp
+name_2 = gets.chomp
 
 
-puts "#{player_1}, #{player_2}, #{color_1}"
-display_grid(board)
+puts "#{name_1}, #{name_2}, #{color_1}"
+
+print board.display_grid
+9.times {
+    puts "Player 1 select a Position"
+    player1_pos = gets.chomp.to_i
+  until board.valid_move?(player1_pos)
+    puts "Enter a valid move"
+    player1_pos = gets.chomp.to_i
+  end
+  puts board.display_grid(player1_pos,color_1)
+
+puts "Player 2 select the position"
+player2_pos = gets.chomp.to_i
+until board.valid_move?(player2_pos)
+  puts "Enter a valid move"
+  player2_pos = gets.chomp.to_i
+end
+puts board.display_grid(player2_pos,color_2)
+
+}
--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,34 @@
 class Board
-  attr_accessor :arr
-  def initialize(arr)
-    @arr = arr
+  attr_accessor :position
+  def initialize
+    @position = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  end
+  
+  def get_color(color)
+    if color == "X"
+      return "O"
+    else
+      return "X"
+    end
+  end
+
+  def valid_move?(move)
+    if @position[move-1] == "X" || @position[move-1] == "O"
+      return false
+    else
+      return true
+    end
+  end
+
+  def display_grid(move=nil,color=nil)
+    if move != nil
+      @position[@position.index(move)] = color
+    end
+    "     #{position[0]} | #{position[1]} | #{position[2]}
+    ---+---+---
+     #{position[3]} | #{position[4]} | #{position[5]} 
+    ---+---+---
+     #{position[6]} | #{position[7]} | #{position[8]}
+     "
   end
 end
